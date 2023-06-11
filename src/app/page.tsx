c405a3e2bf89f06056bbcded8b90a6cbb3e61afe
신선종 사원 스마트물류솔루션팀 (sj.shin@lgcns.com <sj.shin@CNS.LGCNS.COM>
2023-06-12 02:19:00 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import axios from 'axios'
 import Image from 'next/image'
 import Link from "next/link"
 
 export default function Home() {
   
   let name = 'shin2'
-  let link = 'http://google.com'
   // Return 소괄호 안에는 하나의 Html 태그만 존재해야함.
   return (
     <div>
@@ -14,17 +12,3 @@ export default function Home() {
     </div>
   )
 }
-export async function getServerSideProps(context) {
-  const options = {
-      method:'GET',
-      url:`https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE}`,
-      headers:{
-          sccpet:'application/json',
-          'Notion-version' : '2022-06-28',
-          'authorization':process.env.NOTION_API_KEY
-      }
-  };
-
-  const response = await axios.request(options);
-  console.log(response);
-};
