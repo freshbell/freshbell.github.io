147d1a4e983d445cbcd52464198645bd4f7b1531
신선종 사원 스마트물류솔루션팀 (sj.shin@lgcns.com <sj.shin@CNS.LGCNS.COM>
2023-06-19 22:45:43 +0900

    add light

--- src/app/three/08/ex01.js
+++ src/app/three/08/ex01.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// ----- 주제: MeshBasicMaterial
+import dat from 'dat.gui';
 
 export default function example(canvasRef) {
 	// Renderer
@@ -11,7 +11,8 @@ export default function example(canvasRef) {
 	});
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
-
+	renderer.shadowMap.enabled = true; // 그림자 설정 -> light도 설정해야함
+	renderer.shadowMap.type = THREE.BasicShadowMap;
 	// Scene
 	const scene = new THREE.Scene();
 
@@ -22,35 +23,86 @@ export default function example(canvasRef) {
 		0.1,
 		1000
 	);
-	// camera.position.y = 1.5;
+	camera.position.y = 1.5;
 	camera.position.z = 4;
 	scene.add(camera);
 
 	// Light
+	// 똑같이 은은하게 비춰줌 - 단독으로 사용하지 않고 다른 조명이랑 함꼐
 	const ambientLight = new THREE.AmbientLight('white', 0.5);
 	scene.add(ambientLight);
 
-	const directionalLight = new THREE.DirectionalLight('white', 1);
-	directionalLight.position.x = 1;
-	directionalLight.position.z = 2;
-	scene.add(directionalLight);
+	// 태양광 같은 조명
+	const light = new THREE.DirectionalLight('red', 0.5);
+	light.position.y = 3;
+	scene.add(light);
+
+	// 그림자 설정 -> 메쉬도 설정해야함.
+	light.castShadow = true;
+	
+	// 그림자 해상도
+	light.shadow.mapSize.width = 512;
+	light.shadow.mapSize.height = 512;
+	light.shadow.radius = 5;
+	// light helper
+	const lightHelper = new THREE.DirectionalLightHelper(light);
+	scene.add(lightHelper);
 
 	// Controls
 	const controls = new OrbitControls(camera, renderer.domElement);
 
-	// Mesh
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshBasicMaterial({ // 입체감이 없음
-		color: 'orange'
-	});
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+	// geometry
+	const planeGeometry = new THREE.PlaneGeometry(10, 10);
+	const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	const sphereGeometry = new THREE.SphereGeometry(0.7, 16, 16);
+	
+	// material
+	const material1 = new THREE.MeshStandardMaterial({color: 'white'});
+	const material2 = new THREE.MeshStandardMaterial({color: 'royalblue'});
+	const material3 = new THREE.MeshStandardMaterial({color: 'gold'});
+
+	// mesh
+	const plane = new THREE.Mesh(planeGeometry, material1);
+	const box = new THREE.Mesh(boxGeometry, material2);
+	const sphere = new THREE.Mesh(sphereGeometry, material3);
+
+	// set
+	plane.rotation.x = -Math.PI * 0.5;
+	box.position.set(1, 1, 0);
+	sphere.position.set(-1, 1, 0);
+
+	// 그림자 설정
+	plane.receiveShadow = true;
+	box.castShadow = true;
+	box.receiveShadow = true;
+	sphere.castShadow = true;
+	sphere.receiveShadow = true;
+
+	scene.add(plane, box, sphere);
+
+	// AxesHelper
+	const axesHelper = new THREE.AxesHelper(3);
+	scene.add(axesHelper);
+
+	// Dat GUI
+	const gui = new dat.GUI();
+	gui.add(camera.position, 'x', -5, 5, 0.1).name('카메라 X');
+	gui.add(camera.position, 'y', -5, 5, 0.1).name('카메라 Y');
+	gui.add(camera.position, 'z', 2, 10, 0.1).name('카메라 Z');
+
+	gui.add(light.position, 'x', -5, 5, 0.1).name('라이트 X');
+	gui.add(light.position, 'y', -5, 5, 0.1).name('라이트 Y');
+	gui.add(light.position, 'z', -5, 5, 0.1).name('라이트 Z');
 
 	// 그리기
 	const clock = new THREE.Clock();
 
 	function draw() {
-		const delta = clock.getDelta();
+		// const delta = clock.getDelta();
+		const time = clock.getElapsedTime();
+
+		// light.position.x = Math.cos(time) * 5;
+		// light.position.z = Math.sin(time) * 5;
 
 		renderer.render(scene, camera);
 		renderer.setAnimationLoop(draw);
@@ -59,7 +111,6 @@ export default function example(canvasRef) {
 	function setSize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
-		
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.render(scene, camera);
 	}
